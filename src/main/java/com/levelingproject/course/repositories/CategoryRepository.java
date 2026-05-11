73840a89b83fc07555ff8a8a0676d9ac1e51e25b
vsbreno <brenovinicius.santos0@gmail.com>
2026-05-11 17:27:06 -0300

    Category entity

---
+++ src/main/java/com/levelingproject/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.levelingproject.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.levelingproject.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
