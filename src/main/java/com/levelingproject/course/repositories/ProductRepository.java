b67cc90245296c15177ebbb92e033784ba4f2d1e
vsbreno <brenovinicius.santos0@gmail.com>
2026-05-12 11:13:34 -0300

    Product Entity

---
+++ src/main/java/com/levelingproject/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.levelingproject.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.levelingproject.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
