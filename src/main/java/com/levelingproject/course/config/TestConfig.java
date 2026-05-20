beac9812e04585ea8e46770edab1ced8490ea996
vsbreno <brenovinicius.santos0@gmail.com>
2026-05-19 21:05:19 -0300

    Payment, one-to-one association

--- src/main/java/com/levelingproject/course/config/TestConfig.java
+++ src/main/java/com/levelingproject/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.levelingproject.course.entities.Category;
 import com.levelingproject.course.entities.Order;
 import com.levelingproject.course.entities.OrderItem;
+import com.levelingproject.course.entities.Payment;
 import com.levelingproject.course.entities.Product;
 import com.levelingproject.course.entities.User;
 import com.levelingproject.course.entities.enums.OrderStatus;
@@ -78,5 +79,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
